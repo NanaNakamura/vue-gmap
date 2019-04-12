a761a14ae208667da412ff6bae1242da6e5a1c7e
nakamura <nakamura@1-10.com>
2019-04-12 12:39:40 +0900

    調整

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,297 @@
     <h1>Display google map</h1>
 
     <h2>Normal</h2>
-    <Gmap :lat="35.003248" :lng="135.758471"></Gmap>
+    <Gmap
+      :lat="35.003248"
+      :lng="135.758471"
+    />
+
+    <h2>Set zoom</h2>
+    <Gmap
+      :lat="35.003248"
+      :lng="135.758471"
+      :zoom="19"
+    />
 
     <h2>Custom pin</h2>
-    <Gmap :lat="35.003248" :lng="135.758471" maker-src="static/assets/images/maker.svg" :makerWidth="60" :makerHeight="84"></Gmap>
+    <Gmap
+      :lat="35.003248"
+      :lng="135.758471"
+      makerSrc="static/assets/images/maker.svg"
+      :makerWidth="60"
+      :makerHeight="84"
+    />
 
-    <h2>Custom map color</h2>
-    <Gmap :lat="35.003248" :lng="135.758471" type="GrayScaleMap" maker-src="static/assets/images/maker.svg" :makerWidth="60" :makerHeight="84"></Gmap>
+    <h2>Custom map color (and custom pin)</h2>
+    <Gmap
+      :lat="35.003248"
+      :lng="135.758471"
+      type="GrayScaleMap"
+      makerSrc="static/assets/images/maker.svg"
+      :makerWidth="60"
+      :makerHeight="84"
+    />
+
+    <h2>Custom map style (and custom pin)</h2>
+    <p><a href="https://mapstyle.withgoogle.com/" target="_blank">https://mapstyle.withgoogle.com/</a> で作ったstyle jsonを使う</p>
+    <Gmap
+      :lat="35.622929"
+      :lng="139.749772"
+      makerSrc="static/assets/images/maker.svg"
+      :makerWidth="60"
+      :makerHeight="84"
+      type="Custom"
+      :styleJson='[
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ebe3cd"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#523735"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#f5f1e6"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#c9b2a6"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#dcd2be"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#ae9e90"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape.natural",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#93817c"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.business",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#a5b076"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#447530"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f1e6"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#fdfcf8"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f8c967"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#e9bc62"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e98d58"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#db8555"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#806b63"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#8f7d77"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#ebe3cd"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dfd2ae"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#b9d3c2"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#92998d"
+            }
+          ]
+        }
+      ]'
+    />
   </div>
 </template>
 
@@ -53,6 +337,13 @@ export default {
     margin-bottom: 20px;
     font-size: 1.5em;
     font-weight: bold;
+  }
+  p {
+    margin: 10px 0;
+    line-height: 1.4;
+  }
+  a {
+    color: #42b983;
   }
 }
 </style>
--- src/components/Gmap.vue
+++ src/components/Gmap.vue
@@ -18,25 +18,41 @@ export default {
   props: {
     lat: Number,
     lng: Number,
+    zoom: Number,
     makerSrc: String,
     makerWidth: Number,
     makerHeight: Number,
-    type: String
+    type: String,
+    styleJson: Array
   },
   methods: {
     init () {
       let self = this
-      let lat = this.lat
-      let lng = this.lng
-      let makerSrc = this.makerSrc
-      let makerWidth = this.makerWidth
-      let makerHeight = this.makerHeight
-      let style = this.type
+      let lat = self.lat
+      let lng = self.lng
+      let zoom = self.zoom
+      let makerSrc = self.makerSrc
+      let makerWidth = self.makerWidth
+      let makerHeight = self.makerHeight
+      let style = self.type
+      let json = self.styleJson
+
+      // ズームの値
+      if(zoom) {
+        self.data.zoom = zoom
+      }
 
       // 地図を表示
       self.data.map = new google.maps.Map(self.$el, {
         center: {lat: lat, lng: lng},
-        zoom: self.data.zoom
+        zoom: self.data.zoom,
+        // コントローラーの設定
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false
       });
 
       // 地図をグレースケールに
@@ -49,6 +65,13 @@ export default {
         let mapType = new google.maps.StyledMapType(mapStyle);
         self.data.map.mapTypes.set( 'GrayScaleMap', mapType);
         self.data.map.setMapTypeId( 'GrayScaleMap' );
+      }
+      // 地図をCustomに
+      if(style === 'Custom') {
+        let mapStyle = json;
+        let mapType = new google.maps.StyledMapType(mapStyle);
+        self.data.map.mapTypes.set( 'CustomMap', mapType);
+        self.data.map.setMapTypeId( 'CustomMap' );
       }
 
       // ピンを立てる
